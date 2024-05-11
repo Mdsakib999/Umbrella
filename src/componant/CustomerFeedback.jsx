3b35db4c4683fb2aa803207494a02ec732aeb7db
Mdsakib999 <sakibabrar999@gmail.com>
2024-05-11 12:35:35 +0600

    why choose?

---
+++ src/componant/CustomerFeedback.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+
+const CustomerFeedback = () => {
+  const feedbackData = [
+    {
+      text: `Lorem ipsum dolor sit amet consectetur adipi sicing elit. Dolores, ducimus. adipi sicing elit. Dolores, ducimus.`,
+      name: "Abdul Hakim",
+      title: "Feed Miller",
+      image:
+        "https://img.freepik.com/free-photo/awkward-smiling-man-feel-indecisive_176420-18009.jpg?w=996&t=st=1701583573~exp=1701584173~hmac=0034ab6010aedc19e1c2bb3f3a7a37b59c533313a7c7b89ad9c6e06b70c72ed1",
+    },
+    {
+      text: `Lorem ipsum dolor sit amet consectetur adipi sicing elit. Sed nec ipsum enim.  adipi sicing elit. Dolores, ducimus.`,
+      name: "John Doe",
+      title: "Hatchery Owner",
+      image:
+        "https://attractmorematches.com/wp-content/uploads/2023/10/Screenshot-2023-10-18-at-5.08.47-PM.png",
+    },
+    {
+      text: `Lorem ipsum dolor sit amet consectetur adipi sicing elit. Nullam nec semper libero.  adipi sicing elit. Dolores, ducimus.`,
+      name: "Angelina",
+      title: "Poultry Owner",
+      image:
+        "https://qph.cf2.quoracdn.net/main-qimg-f1f534fd992d4bbbd1b5c2bfb0b640a5-lq",
+    },
+  ];
+
+  const [currentFeedbackIndex, setCurrentFeedbackIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentFeedbackIndex(
+        (prevIndex) => (prevIndex + 1) % feedbackData.length
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [currentFeedbackIndex, feedbackData.length]);
+
+  const currentFeedback = feedbackData[currentFeedbackIndex];
+
+  return (
+    <div className=" bg-slate-100 py-20">
+      
+
+      {/* Feedback */}
+
+      <div>
+        <div className="w-full  mx-auto px-7 py-4 lg:py-7    text-lg text-center"> 
+        <span className="text-5xl font-mono text-sky-700">"</span>
+        <h1 className="mb-3">
+          {currentFeedback.text}
+        </h1>
+        <span className="text-5xl font-mono text-sky-700">"</span>
+
+        </div>
+        
+
+        {/*  */}
+
+        <div className=" flex items-center justify-center gap-x-4 my-4">
+          <div className="w-[60px] h-[60px] rounded-full border-4 border-blue-500">
+            <img
+              className="rounded-full h-full w-full"
+              src={currentFeedback.image}
+              alt=""
+            />
+          </div>
+
+          <div className="my-5">
+            <p className="font-bold">{currentFeedback.name}</p>
+            <p className="font-semibold">{currentFeedback.title}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerFeedback;
--- src/componant/Home.jsx
+++ src/componant/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardHomeInfo from './CardHomeInfo';
+import CustomerFeedback from './CustomerFeedback';
 
 const Home = () => {
     return (
@@ -14,8 +15,11 @@ const Home = () => {
 
         <CardHomeInfo></CardHomeInfo>
 
+        {/* user Review */}
+        <CustomerFeedback></CustomerFeedback>
+
         {/* home about */}
-        <div className=' lg:py-16 flex justify-evenly items-center gap-8'>
+        <div className=' lg:py-24 flex justify-evenly items-center gap-8'>
             <div className='lg:w-[40%]'>
                 <p>ABOUT US</p>
                 <p className='text-4xl font-serif my-5'>WE ARE AN INDEPENDENT INSURANCE BROKER</p>
@@ -27,6 +31,39 @@ const Home = () => {
                 <img className='w-full' src="https://i.ibb.co/xMFYMRd/umblrlla-Logo.jpg" alt="About picture" />
             </div>
         </div>
+
+
+        {/* Why choice us */}
+
+        <div
+      className="relative overflow-hidden bg-cover bg-no-repeat p-12 text-center"
+      style={{ backgroundImage: "url('https://img.freepik.com/free-photo/man-giving-thumbs-up-out-his-car-while-road-trip_23-2148783405.jpg?w=1060&t=st=1715407558~exp=1715408158~hmac=3821bb32e3a21ae74c2048b71f328b625bd2d62c245c342233dd7df32252931e')", height: '600px' }}
+    >
+      <div
+        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
+        <div className=" flex h-full items-center justify-center">
+          <div className=" text-white">
+            <h2 className="mb-4 text-6xl text-blue-600 font-semibold">Why choose us?</h2>
+            <ol className='text-left mt-10 mb-12 text-xl font-semibold leading-10'>
+                <li>1. Specialized quotation team to search the best out of the market</li>
+                <li>2. In most cases we are 40% cheaper than whatever you find yourself</li>
+                <li>3. Up-to 4 free quotation </li>
+                <li>4. Free customer service for 12 months</li>
+                <li>5. Multilingual Customer Service team</li>
+            </ol>
+            <button
+              type="button"
+              className="rounded border-2 border-blue-600 bg-blue-600 px-7 pb-[8px] pt-[10px] font-medium leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 "
+              
+            >
+              Get a Quote Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
         </div>
